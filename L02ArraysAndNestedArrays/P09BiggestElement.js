4c4d47c6658c206e4bd5a57e703dfc7c901f6a26
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-05-24 00:01:33 +0300

    Add files via upload

---
+++ L02ArraysAndNestedArrays/P09BiggestElement.js
@@ -0,0 +1,19 @@
+function solve(matrix){
+    let maxElement = matrix[0][0];
+    for(let i = 1; i < matrix.length; i++){
+        for(let j = 1; j < matrix[i].length; j++){
+            if(Number(matrix[i][j]) > Number(maxElement)){
+                maxElement = matrix[i][j];
+            }
+        }
+    }
+    return maxElement;
+}
+
+console.log(solve([[20, 50, 10],
+    [8, 33, 145]]
+   ));
+console.log(solve([[3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]]
+   ));
