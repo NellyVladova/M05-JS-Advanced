6008753a6c336d1ba7bdb54d3d724b28a4c3f5b4
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-05-23 23:33:26 +0300

    Add files via upload

---
+++ L02ArraysAndNestedArrays/P07PieceOfPie.js
@@ -0,0 +1,21 @@
+function solve(arr, str1, str2){
+     return arr.slice(arr.indexOf(str1), arr.indexOf(str2) + 1);
+}
+
+console.log(solve(['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie'
+));
+console.log(solve(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie'
+));
