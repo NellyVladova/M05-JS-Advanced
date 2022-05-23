caf4338c503d1725ca2cef3dc124bac34e46c001
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-05-23 23:46:33 +0300

    Delete P08ProcessOddPositions.js

--- P08ProcessOddPositions.js
+++
@@ -1,6 +0,0 @@
-function solve(arr) {
-    return arr.filter((value, index) => index % 2 != 0).map( el => el * 2).reverse();
-}
-
-console.log(solve([10, 15, 20, 25]));
-console.log(solve([3, 0, 10, 4, 7, 3]));
