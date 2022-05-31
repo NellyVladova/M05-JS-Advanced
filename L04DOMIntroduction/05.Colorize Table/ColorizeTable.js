48d34d025f562ea129fe0d80a862143a677e232f
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-06-01 00:12:59 +0300

    Add files via upload

---
+++ L04DOMIntroduction/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,7 @@
+function colorize() {
+    // TODO
+    const rows = document.querySelectorAll('tr');
+    for (let i = 1; i < rows.length; i += 2) {
+        rows[i].style.background = 'teal';
+    }
+}
