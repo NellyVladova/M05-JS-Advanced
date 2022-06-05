7848adc5fa8bc6c6d11a68cfed11f867888f12ac
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-06-05 18:30:53 +0300

    Delete L05DOMManipulationsAndEvents/01. List-Of-Items directory

--- L05DOMManipulationsAndEvents/01. List-Of-Items/app.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-    let newLi = document.createElement('li');
-    let addText = document.getElementById('newItemText').value;
-    let listWithItems = document.getElementById('items');
-
-    newLi.appendChild(document.createTextNode(addText));
-    listWithItems.appendChild(newLi);
-}
