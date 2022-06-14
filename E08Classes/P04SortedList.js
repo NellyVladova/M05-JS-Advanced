192a7f7274dcc65671103d76ee1513dcc7f792ae
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-06-14 23:19:51 +0300

    Add files via upload

---
+++ E08Classes/P04SortedList.js
@@ -0,0 +1,31 @@
+class List{
+    constructor(){
+        this.array = [];
+        this.size = this.array.length;
+    }
+    add(element){
+        this.array.push(element);
+        this.array.sort((el1, el2) => el1 - el2);
+        this.size = this.array.length;
+    }
+    remove(index){
+        if(index >= 0 && index <= this.array.length - 1){
+            this.array.splice(index, 1);
+            this.array.sort((el1, el2) => el1 - el2);
+            this.size = this.array.length;
+        }
+    }
+    get(index){
+        if(index >= 0 && index <= this.array.length - 1){
+            return this.array[index];
+        }
+    }
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1)); 
+list.remove(1);
+console.log(list.get(1));
